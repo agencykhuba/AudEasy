f17327d344299f525541dd2d20fdfbb532754ba0
Dennis James Barba <agencykhuba@gmail.com>
2025-09-05 09:56:38 -0300

    Reinitialize AudEasy frontend with Express

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,1 @@
-const express = require('express'); const app = express(); app.get('/', (req, res) => res.send('AudEasy Frontend')); app.listen(3000);
+const express = require('express'); const app = express(); app.get('/', (req, res) => res.send('AudEasy Frontend')); app.listen(3000, () => console.log('AudEasy server running on port 3000'));
