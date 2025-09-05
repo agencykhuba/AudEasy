4f300b5b05cce1879e59916a64cb71aaa9c56f87
Dennis James Barba <agencykhuba@gmail.com>
2025-09-05 10:09:26 -0300

    Add API route to AudEasy frontend

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,5 @@
-const express = require('express'); const app = express(); app.get('/', (req, res) => res.send('AudEasy Frontend')); app.listen(3000, () => console.log('AudEasy server running on port 3000'));
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('AudEasy Frontend'));
+app.get('/api', (req, res) => res.json({ message: 'AudEasy API' }));
+app.listen(3000, () => console.log('AudEasy server running on port 3000'));
