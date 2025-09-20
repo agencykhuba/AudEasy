067d50dcf95cf557e8279dd668e12bcecf5ff0b8
Dennis James Barba <agencykhuba@gmail.com>
2025-09-20 02:08:51 -0300

    Fix: Remove file logging causing deployment failure

--- code/app.py
+++ code/app.py
@@ -10,7 +10,6 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
-        logging.FileHandler("logs/app.log"),
         logging.StreamHandler()
     ]
 )
